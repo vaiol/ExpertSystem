d0ccdd261c8f872d3dac0440bf11861e587dd532
mbodak <mbodak@student.unit.ua>
2019-04-30 12:54:30 +0300

    rule completed

--- logic/classes/Rule.js
+++ logic/classes/Rule.js
@@ -1,13 +1,50 @@
 const { Algorithm } = require('./Alghorithm');
 const { Fact } = require('./Fact');
 
-const extractNewFacts = (str) => {};
+const extractNewFacts = (str) => {
+    const removeList = ['!', '+', '|', '^', '(', ')', '\n'];
+    for (let char of removeList) {
+        if (str.includes(char)) {
+            str = str.replace(char, ' ');
+        }
+    }
+    return str.split(' ');
+};
 
 
 class Rule {
-    constructor(rule, parent) {}
-    processRule(self) {}
-	collectNewFacts(self, facts, implication) {}
+    constructor(rule, parent) {
+        this.condition = '';
+		this.implication = '';
+
+		this.parent = parent;
+		this.rule = rule;
+		this.algorithm = new Algorithm(this.rule);
+		this.processRule();
+    }
+
+    processRule() {
+        this.condition = this.algorithm.shuntingYard(this.algorithm.condition);
+		this.implication = this.algorithm.shuntingYard(this.algorithm.implication);
+
+		let facts = extractNewFacts(this.algorithm.condition);
+		this.collectNewFacts(facts, false);
+		facts = extractNewFacts(this.algorithm.implication);
+		this.collectNewFacts(facts, true);
+    }
+
+    collectNewFacts(facts, implication) {
+        for (let fact of facts) {
+            if (fact.length === 1) {
+                if (!this.parent.facts.includes(fact)) {
+                    this.parent.facts[fact] = new Fact(fact, this.parent);
+                }
+                if (implication) {
+                    this.parent.facts[fact].dependencies.push(this);
+                }
+            }
+        }
+    }
 }
 
 module.exports = {
